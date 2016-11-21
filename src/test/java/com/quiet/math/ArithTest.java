b021f049ff49f6a54f96b932e5c03b5c44cb3c88
jiawei <jiawei@sohu-inc.com>
2016-11-21 18:34:00 +0800

    增加数学方法，求解公约数

---
+++ src/test/java/com/quiet/math/ArithTest.java
@@ -0,0 +1,18 @@
+package com.quiet.math;
+
+import org.junit.Test;
+
+/**
+ * Copyright tv.sohu.com
+ * Author : jiawei
+ * Date   : 2016/11/21
+ * Desc   :
+ */
+public class ArithTest {
+
+    @Test
+    public void gcdTest(){
+        System.out.println(Arith.gcd(160,-300));
+    }
+
+}
